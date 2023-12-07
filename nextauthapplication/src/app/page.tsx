68346de05a631c095012136c7049fda74d6474a4
Azalmonawwar <monawwarazal@gmail.com>
2023-12-07 22:07:07 +0530

    added middleware in auth

--- nextauthapplication/src/app/page.tsx
+++ nextauthapplication/src/app/page.tsx
@@ -1,7 +1,11 @@
-import Image from 'next/image'
+
 
 export default function Home() {
   return (
-    <></>
+    <>
+      <h1 className='h-screen flex items-center justify-center'>
+        Welcome to Home page
+      </h1>
+    </>
   )
 }
