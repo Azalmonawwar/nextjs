32daf527c969c64392629ca0ee4ff947c25b35a3
Azalmonawwar <monawwarazal@gmail.com>
2023-12-08 14:28:56 +0530

    added logout function

---
+++ nextauthapplication/src/app/api/user/logout/route.ts
@@ -0,0 +1,21 @@
+import { NextResponse,NextRequest } from "next/server";
+
+//logout function
+
+export async function GET(request:NextRequest){
+    try {
+        const response = NextResponse.json(
+            {
+                message: "Logout successful",
+                success: true,
+            }
+        )
+        response.cookies.set("token", "", 
+        { httpOnly: true, expires: new Date(0) 
+        });
+        return response;
+    } catch (error) {
+        return NextResponse.json({message :'Something went wrong'},{status:500});
+    }
+    
+}
--- nextauthapplication/src/app/page.tsx
+++ nextauthapplication/src/app/page.tsx
@@ -1,38 +1,56 @@
 'use client'
 import axios from "axios"
+import { useRouter } from "next/navigation"
 import { useState, useEffect } from "react"
+
 type Data = {
   email: string;
   name: string;
   _id: string;
 }
+
 export default function Home() {
   const [data, setData] = useState<Data>({
     email: '',
     name: '',
     _id: '',
   })
-  const getUser = async () => {
 
+  const router = useRouter()
+
+  const getUser = async () => {
     try {
       const { data } = await axios.get('/api/user')
+      // console.log(data)
       setData(data.data)
-      console.log(data)
+      // return await data.json()
     } catch (error) {
       console.log(error)
     }
   }
 
+  const logout = async () => {
+    try {
+      const { data } = await axios.get('/api/user/logout')
+
+      console.log(data)
+      router.push('/login')
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   useEffect(() => {
     getUser()
   }, [])
   return (
-    <>
-      <h1 className='h-screen flex items-center justify-center'>
+    <section className="h-screen flex flex-col items-center justify-center">
+      <h1 className=' '>
         Welcome to Home page
         <p className="text-xl font-bold capitalize ml-3">{data?.name}</p>
+
       </h1>
-    </>
+      <button className="text-sm px-4 py-2 bg-blue-300 text-black rounded-md" onClick={logout}>Logout</button>
+    </section>
   )
 }
