2e7548bc965cc533f7cfa9e2340be41b34555bc9
Azalmonawwar <monawwarazal@gmail.com>
2023-12-25 11:40:57 +0530

    add env variable to db

--- ecommerce/src/db/dbConnect.ts
+++ ecommerce/src/db/dbConnect.ts
@@ -1,22 +1,24 @@
-// function to connect with mongo db 
+// function to connect with mongo db
 
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const MONGODB_URI = 'mongodb+srv://monawwarazal:iamazal123@cluster0.gmpuy7v.mongodb.net/';
+const MONGODB_URI = process.env.MONGODB_URI;
 
 let cached = (global as any).mongoose || { conn: null, promise: null };
 
 export const connectToDatabase = async () => {
   if (cached.conn) return cached.conn;
 
-  if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
+  if (!MONGODB_URI) throw new Error("MONGODB_URI is missing");
 
-  cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
-    dbName: 'ecommerce',
-    bufferCommands: false,
-  })
+  cached.promise =
+    cached.promise ||
+    mongoose.connect(MONGODB_URI, {
+      dbName: "ecommerce",
+      bufferCommands: false,
+    });
 
   cached.conn = await cached.promise;
 
   return cached.conn;
-}
+};
