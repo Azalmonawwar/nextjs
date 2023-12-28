5d829cc4342900281747f0318c97318323a66571
Azalmonawwar <monawwarazal@gmail.com>
2023-12-28 14:21:44 +0530

    add api route for testin

--- ecommerce/src/app/api/route.ts
+++ ecommerce/src/app/api/route.ts
@@ -1,7 +1,13 @@
+import { connectToDatabase } from "@/db/dbConnect";
+import Product from "@/models/products.model";
 import { NextRequest,NextResponse } from "next/server";
 
-export async function GET(request : NextRequest){
+export async function POST(request:NextRequest){
+    await connectToDatabase();
+    const {id,title,price,rating,category,department,image} = await request.json();
+    const data = {id,title,price,rating,category,department,image};
+    const product = await Product.create(data);
     return NextResponse.json({
-        message : "Hello World"
-    })
+        message:"Product created successfully",
+        product});
 }
