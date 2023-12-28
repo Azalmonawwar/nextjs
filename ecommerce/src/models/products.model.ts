4bbeff95e2b9f3e5491369e346ac1f12b8b8602d
Azalmonawwar <monawwarazal@gmail.com>
2023-12-28 15:44:20 +0530

    last changes in product model

--- ecommerce/src/models/products.model.ts
+++ ecommerce/src/models/products.model.ts
@@ -9,8 +9,6 @@ export interface Product extends Document {
   image: string;
   category: string;
   department: string;
-  createdAt: Date;
-  updatedAt: Date;
 }
 
 const productSchema = new Schema<Product>(
